7b062521f448eaeec1cd5f12c1e2c6e9262c960e
Marissa <marissa@gmail.com>
2022-08-22 23:07:20 -0500

    Este es el segundo cambio a realizar

--- Mundo.py
+++ Mundo.py
@@ -1,5 +1,5 @@
 
 imprimir  "Hola mundo!"
-print  "Me esta gustando Python"
+print  "Me esta gustando Python, SEGUNDO EJEMPLO DEL CAMBIO"
 nombre  =  raw_input ( "Dime como te llamas: " )
 imprimir  "Encantado de saludarte " , nombre
